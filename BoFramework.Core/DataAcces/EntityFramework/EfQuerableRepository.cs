fccc7b5b3741e088086cc28cbf561a0bb105f91d
Berkay Öztürk <berkayozturk7801@gmail.com>
2022-03-08 16:31:13 +0300

    Adding ORM technologies to the core layer
    Adding Entity Framework to the project. Implementing data access interfaces.

---
+++ BoFramework.Core/DataAcces/EntityFramework/EfQuerableRepository.cs
@@ -0,0 +1,28 @@
+﻿using BoFramework.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoFramework.Core.DataAcces.EntityFramework
+{
+    public class EfQuerableRepository<T> : IQueryableRepository<T> where T : class, IEntity, new()
+    {
+        private DbContext _context;
+        private IDbSet<T> _entities;
+
+        public EfQuerableRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<T> Table => this.Entities;
+
+        public virtual IDbSet<T> Entities
+        {
+            get { return _entities ?? (_entities = _context.Set<T>());  }
+        }
+    }
+}
