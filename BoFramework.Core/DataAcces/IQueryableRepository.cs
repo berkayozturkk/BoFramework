90c761ebf371389d1a8227e84f033ba39e00062f
Berkay Öztürk <berkayozturk7801@gmail.com>
2022-03-08 15:55:27 +0300

    Data access interfaces
    Necessary methods for data access interfaces are written. General structures and restrictions on type parameters

---
+++ BoFramework.Core/DataAcces/IQueryableRepository.cs
@@ -0,0 +1,14 @@
+﻿using BoFramework.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoFramework.Core.DataAcces
+{
+    public interface IQueryableRepository<T> where T:class, IEntity, new()
+    {
+        IQueryable<T> Table { get; }
+    }
+}
