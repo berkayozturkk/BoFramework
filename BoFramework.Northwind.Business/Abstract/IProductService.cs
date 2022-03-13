408b7ab6780e786f4ca334b2995081315e93b7c7
Berkay Öztürk <berkayozturk7801@gmail.com>
2022-03-13 21:31:49 +0300

    Business layer implementation
    Interface and class created.Interface is used to eliminate dependency.

---
+++ BoFramework.Northwind.Business/Abstract/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using BoFramework.Northwind.Entities.Concrate;
+using System.Collections.Generic;
+
+namespace BoFramework.Northwind.Business.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetAll();
+        Product GetById(int id);
+        Product Add(Product product);
+    }
+}
---
+++ BoFramework.Northwind.Business/Concrate/Managers/ProductManager.cs
@@ -0,0 +1,32 @@
+﻿using BoFramework.Northwind.Business.Abstract;
+using BoFramework.Northwind.DataAccess.Abstract;
+using BoFramework.Northwind.Entities.Concrate;
+using System.Collections.Generic;
+
+namespace BoFramework.Northwind.Business.Concrate.Managers
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public Product Add(Product product)
+        {
+            return _productDal.Add(product);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetList();
+        }
+
+        public Product GetById(int id)
+        {
+            return _productDal.Get(p => p.ProductId == id);
+        }
+    }
+}
