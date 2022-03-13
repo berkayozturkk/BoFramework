3f7b042100d9d29b51dbcea24265672e39879fc0
Berkay Öztürk <berkayozturk7801@gmail.com>
2022-03-13 19:08:15 +0300

    Framework usage. End-to-end implementation of business rules for NHibernate
    Optional mapping is done. The interface where the methods are written and the class where the operations are performed are inherited. The constructor method is used with the NHibernateHelper object to implement the dependency injection method of the software in the Core layer. This is a business rule.

---
+++ BoFramework.Northwind.DataAccess/Concrete/NHibernate/Mappings/CategoryMap.cs
@@ -0,0 +1,21 @@
+﻿using BoFramework.Northwind.Entities.Concrate;
+using FluentNHibernate.Mapping;
+
+namespace BoFramework.Northwind.DataAccess.Concrete.NHibernate.Mappings
+{
+    public class CategoryMap : ClassMap<Category>
+    {
+
+        public CategoryMap()
+        {
+            Table(@"Categories");
+
+            LazyLoad();
+
+            Id(x => x.CategoryId).Column("CategoryId");
+
+            Map(x => x.CategoryName).Column("CategoryName");
+            Map(x => x.Description).Column("Description");
+        }
+    }
+}
---
+++ BoFramework.Northwind.DataAccess/Concrete/NHibernate/NhCategoryDal.cs
@@ -0,0 +1,13 @@
+﻿using BoFramework.Core.DataAcces.NHibernate;
+using BoFramework.Northwind.DataAccess.Abstract;
+using BoFramework.Northwind.Entities.Concrate;
+
+namespace BoFramework.Northwind.DataAccess.Concrete.NHibernate
+{
+    public class NhCategoryDal : NhEntityRepositoryBase<Category>, ICategoryDal
+    {
+        public NhCategoryDal(NHibernateHelper nHibernateHelper) : base(nHibernateHelper)
+        {
+        }
+    }
+}
